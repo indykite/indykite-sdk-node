991fc12ae706f2b688bd253748603e4c33d0ae37
Dušan Kováčik <dusan.kovacik@indykite.com>
2022-07-20 13:15:42 +0200

    fix: export missing import digital twin classes

--- jarvis_sdk_node/src/sdk/model/index.ts
+++ jarvis_sdk_node/src/sdk/model/index.ts
@@ -3,3 +3,5 @@ export * from './token';
 export * from './property';
 export * from './config';
 export * from './consent_challenge';
+export * from './import_digitaltwin';
+export * from './hash_algorithm';
