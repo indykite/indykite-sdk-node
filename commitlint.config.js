13bb7433b6a84c1e55e8ba878500976820ce4e0d
Martin Prokes <martin.prokes@profiq.com>
2022-08-31 15:49:41 +0200

    ci: relax the commitlint rules for subject case

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,7 +7,7 @@ module.exports = {
     "header-max-length": [2, "always", 72],
     "subject-max-length": [2, "always", 50],
     "subject-full-stop": [2, "never", "."],
-    "subject-case": [2, "always", ["lower-case"]],
+    "subject-case": [2, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
     "type-enum": [
       2,
       "always",
