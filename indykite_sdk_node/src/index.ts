15a9047b430b8e11e332654d55540a2fe835bb2e
Dušan Kováčik <dusan.kovacik@indykite.com>
2023-04-14 09:25:50 +0200

    docs: update api documentation

--- indykite_sdk_node/src/index.ts
+++ indykite_sdk_node/src/index.ts
@@ -1,5 +1,5 @@
 export { DigitalTwinKind, DigitalTwinState } from './grpc/indykite/identity/v1beta2/model';
-export { AuthorizationClient } from './sdk/authorization';
+export * from './sdk/authorization';
 export { IdentityClient } from './sdk/identity';
 export { ConfigClient } from './sdk/config';
 export { HTTPClient, HTTPResponse } from './sdk/http';
