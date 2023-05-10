89ef055757ffc44c1e94d133a12594272966bde8
Dušan Kováčik <dusan.kovacik@indykite.com>
2023-05-10 10:57:54 +0200

    feat: new ingest client

--- indykite_sdk_node/src/sdk/client/client.ts
+++ indykite_sdk_node/src/sdk/client/client.ts
@@ -14,7 +14,8 @@ import { SdkErrorCode, SdkError } from '../error';
 import { LIB_VERSION } from '../../version';
 import { IdentityManagementAPIClient } from '../../grpc/indykite/identity/v1beta2/identity_management_api.grpc-client';
 import { ConfigManagementAPIClient } from '../../grpc/indykite/config/v1beta1/config_management_api.grpc-client';
-import { IngestAPIClient } from '../../grpc/indykite/ingest/v1beta1/ingest_api.grpc-client';
+import { IngestAPIClient as IngestAPIClientV1 } from '../../grpc/indykite/ingest/v1beta1/ingest_api.grpc-client';
+import { IngestAPIClient as IngestAPIClientV2 } from '../../grpc/indykite/ingest/v1beta2/ingest_api.grpc-client';
 import { ApplicationCredential } from '../utils/application_credential';
 import { ServiceAccountCredential } from '../utils/service_account_credential';
 import { AuthorizationAPIClient } from '../../grpc/indykite/authorization/v1beta1/authorization_service.grpc-client';
@@ -23,7 +24,8 @@ import { Token } from '../utils/token_source/token';
 type ClientType =
   | IdentityManagementAPIClient
   | ConfigManagementAPIClient
-  | IngestAPIClient
+  | IngestAPIClientV1
+  | IngestAPIClientV2
   | AuthorizationAPIClient;
 type ClientConstructor = new (
   endpoint: string,
