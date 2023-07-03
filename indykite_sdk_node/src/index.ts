1dbc188d8dc97696f5862636feb13dc4e96c7e70
Dušan Kováčik <dusan.kovacik@indykite.com>
2023-07-03 10:44:12 +0200

    feat: new identity client

--- indykite_sdk_node/src/index.ts
+++ indykite_sdk_node/src/index.ts
@@ -1,5 +1,6 @@
 export * as grpcIdentityModel from './grpc/indykite/identity/v1beta2/model';
 export * as grpcIdentityAPI from './grpc/indykite/identity/v1beta2/identity_management_api';
+export * as grpcIdentityAttributes from './grpc/indykite/identity/v1beta2/attributes';
 export * from './sdk/authorization';
 export { IdentityClient } from './sdk/identity';
 export * from './sdk/identity_v2';
@@ -7,7 +8,6 @@ export { ConfigClient } from './sdk/config';
 export { ConfigClientV2 } from './sdk/config_v2';
 export { HTTPClient, HTTPResponse } from './sdk/http';
 export * from './sdk/ingest';
-export * from './sdk/builders';
 export * from './sdk/model';
 export {
   ChangePasswordRequest,
